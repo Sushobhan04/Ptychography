e9e2244ae4ca5fa26cda2890a235069bb7c5882a
Sushobhan04 <sushobhanghosh2021@u.northwestern.edu>
2017-04-04 23:49:21 -0500

    macbook copy

--- create_ptych_dataset.py
+++ create_ptych_dataset.py
@@ -2,6 +2,9 @@ import numpy as np
 import numpy.fft as fft
 import matplotlib.pyplot as plt
 import matplotlib.image as im
+from PIL import Image
+import os
+import h5py
 
 
 def plot_arr(arr,name):
@@ -23,25 +26,50 @@ def patchify(img,size):
 def batch_fft(batch):
 	batch_f = []
 	for x in batch:
-		batch_f.append(np.absolute(fft.fft2(x)))
+		batch_f.append(np.absolute(fft.fftshift(fft.fft2(x))))
 	return batch_f
 
-def create_dataset(source,destination):
+def create_dataset(N,source,destination,dataset_name):
+	dataset = []
+	labelset = []
 
+	for filename in os.listdir(source):
+		img = np.asarray(Image.open(source+filename).convert('L'))/255.0
+		patches = patchify(img,N)
+		patches_fft = batch_fft(patches)
+
+		dataset.extend(patches_fft)
+		labelset.extend(patches)
+
+
+	f = h5py.File(destination+dataset_name+'.h5','w')
+	f['data'] = np.array(dataset)
+	f['label'] = np.array(labelset)
+	f.close()
+
+	print "dataset created"
+
+	plot_arr(labelset[0],'test_img')
+	plot_arr(np.clip(dataset[0],0.0,10.0),'test_fft')
+	print labelset[0], dataset[0]
+	print np.max(dataset[0]), np.min(dataset[0])
+	print len(dataset)
 
 def main():
 	output_path = '/home/sushobhan/Documents/data/ptychography/datasets/'
-	source = '/home/sushobhan/Documents/data/ptychography/data/'
-	N = 64
+	source = '/home/sushobhan/Documents/data/ptychography/data/Set91/'
+	N = 32
 
 	dataset_name = 'patch_'+str(N)
+
+	create_dataset(N,source,output_path,dataset_name)
 
 
 
 	
 
-	plot_arr(arr,'test_img')
-	plot_arr(trans,'test_fft',t='c')
+	# plot_arr(arr,'test_img')
+	# plot_arr(trans,'test_fft',t='c')
 
 if __name__=='__main__':
 	main()
